eeda3821e81c6b941f3c766b34bdf9d7cb54d5aa
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2023-04-03 23:01:53 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -60,8 +60,7 @@ router.post('/signin',async (req,res)=>{
                 res.status(400).json({error:"invalid details"})
             }else{
                 // console.log('welcome')
-                req.headers('Acces-Control-Allow-Origin':'*')
-                res.json('go to home page')
+                res.headers('Acces-Control-Allow-Origin':'*').json('go to home page')
             }
         }else{
             res.status(400).json({error:"invalid details"})
